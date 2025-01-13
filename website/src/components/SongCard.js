ee90cc2b56d2ac80b338bdaeddf757cda0a2ba41
r0831281 - Jonas <r0831281@student.thomasmore.be>
2025-01-13 21:37:07 +0100

    vis improv

--- website/src/components/SongCard.js
+++ website/src/components/SongCard.js
@@ -11,19 +11,21 @@ const SongCard = ({ song }) => {
     <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
       <div className="card mb-4" style={{ borderRadius: '16px', border: 'none', width: '100%' }}>
         <div className="card-body d-flex flex-column">
+          <div className="video-container">
+            {embedUrl && (
+              <div className="ratio ratio-16x9 mt-auto">
+                <iframe
+                  src={embedUrl}
+                  title={song.title}
+                  className="card-img-top"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            )}
+            <p className="artist-text text-muted">By: Andro!dz</p>
+          </div>
           <h3 className="card-title">{song.title}</h3>
-          <p className="card-text text-muted">By: Andro!dz</p>
-          {embedUrl && (
-            <div className="ratio ratio-16x9 mt-auto">
-              <iframe
-                src={embedUrl}
-                title={song.title}
-                className="card-img-top"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
-          )}
         </div>
       </div>
     </div>
